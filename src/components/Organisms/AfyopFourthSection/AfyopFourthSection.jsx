d7ce7dc3b688b3753b43c75c362aaea4b54b2f46
Migs <miguelemmanueldailisan@gmail.com>
2022-06-15 03:18:17 +0800

    updated AFYOP 4th section
    added "now happening"( will fix later)

--- src/components/Organisms/AfyopFourthSection/AfyopFourthSection.jsx
+++ src/components/Organisms/AfyopFourthSection/AfyopFourthSection.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import styles from "./AfyopFourthSection.module.scss";
 import { AFYOPContainer } from '../../ComponentIndex';
 
-const AfyopFourthSection = ({title="Class Finder", variant = "default", children, data}) => {
+const AfyopFourthSection = ({title, variant = "default", children, data}) => {
   return (
     <>
     <div className={styles["component-container"]}>
+    <div className={styles["grid-container"]}>
     <div className="container">
-    <AFYOPContainer title={title} variant={variant} children={children}/>
+    <AFYOPContainer title={title="Class Finder"} variant={variant} children={children}/>
+    </div>
+
+    <div className="container">
+    <AFYOPContainer title={title="Now Happening"} variant={variant} children={children}/>
+    </div>
+    
     </div>
     </div>
     </>
