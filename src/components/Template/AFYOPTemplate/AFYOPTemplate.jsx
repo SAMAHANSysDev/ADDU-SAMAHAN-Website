89f40c74d67ad6300c499358ffc2cea8813a67e8
Migs <miguelemmanueldailisan@gmail.com>
2022-06-16 02:00:00 +0800

    added AFYOP 4th section to template
    added the 4th section to the AFYOP page

--- src/components/Template/AFYOPTemplate/AFYOPTemplate.jsx
+++ src/components/Template/AFYOPTemplate/AFYOPTemplate.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
-import styles from './AFYOPTemplate.module.scss'
-import { Navigation, Footer, AFYOPFeedSection } from '../../ComponentIndex'
+import styles from './AFYOPTemplate.module.scss';
+import { Navigation, Footer, AFYOPFeedSection, AfyopFourthSection } from '../../ComponentIndex';
+import { CFData, NHData } from '../../../data/dataIndex';
+
 
 const AFYOPTemplate = ({ SimulaTVContent }) => {
   return (
     <>
         <Navigation />
         <AFYOPFeedSection variant={SimulaTVContent.variant} content={SimulaTVContent.content}/>
+        <AfyopFourthSection variant={CFData.content.variant} content={CFData.content.items} content2={NHData.content.contents} />
         <Footer />
     </>
   )
