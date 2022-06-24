0d5adad1ac1fa03c2c0ca42015541d6208611977
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2022-06-24 19:34:49 +0800

    Fixed carousel

--- src/components/Molecules/AFYOPCarousel/AFYOPCarousel.jsx
+++ src/components/Molecules/AFYOPCarousel/AFYOPCarousel.jsx
@@ -1,38 +1,70 @@
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { CarouselContent } from "../../ComponentIndex";
+import Image from 'next/image'
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// import { CarouselContent } from "../../ComponentIndex";
 import styles from "./AFYOPCarousel.module.scss";
 
-const settings = {
-  dots: false,
-  fade: true,
-  infinite: true,
-  arrows: false,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  speed: 5000,
-  autoplaySpeed: 2000,
-};
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+// const settings = {
+//   dots: false,
+//   fade: true,
+//   infinite: true,
+//   arrows: false,
+//   speed: 500,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   speed: 5000,
+//   autoplaySpeed: 2000,
+// };
 
 const AFYOPCarousel = ({ carcon }) => {
   return (
-    <div>
-      <div className={styles["sliderContainer"]}>
-        <Slider {...settings}>
-          {carcon.content.map((item, index) => {
-            return (
-              <div key={index} className={styles["slick"]}>
-                <CarouselContent imgSrc={item} />
-              </div>
-            );
-          })}
-        </Slider>
-      </div>
-    </div>
+    // <div>
+    //   <div className={styles["sliderContainer"]}>
+    //     <Slider {...settings}>
+    //       {carcon.content.map((item, index) => {
+    //         return (
+    //           <div key={index} className={styles["slickkk"]}>
+    //             <CarouselContent imgSrc={item} />
+    //           </div>
+    //         );
+    //       })}
+    //     </Slider>
+    //   </div>
+    // </div>
+
+    <>
+      <Swiper
+        pagination={{
+          type: "fraction",
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation, Autoplay]}
+        autoplay={{delay: 5000}}
+        className={styles["mySwiper"]}
+      >
+        {carcon.content.map((item, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <Image src={item} layout="fill" objectFit="contain" alt="Carousel Image" />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
   );
 };
 
