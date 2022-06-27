8bed2385ad1fa3bad2163029d7a5c3f6d3d7cdf8
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2022-06-27 21:12:47 +0800

    secret page

--- pages/sysdev/secret.jsx
+++ pages/sysdev/secret.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React from "react";
 import Head from "next/head";
 
 const secret = () => {
+  const style = {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  const pink = {
+    color: "pink",
+  };
   return (
     <>
       <Head>
         <title>SysDev</title>
       </Head>
-      <div>secret</div>
+      <div style={style}>
+        <h1>WELCOME TO THE SECRET PAGE</h1>
+        <h1 style={pink}>IF YOU FOUND THIS, CONGRATS IG???</h1>
+        <p style={{marginTop: "1rem"}}>Message any SysDev member that you found this for good luck hehe</p>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default secret
+export default secret;
